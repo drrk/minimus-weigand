06918903d5a27951417a1b0ff4b1f15f5fc4344f
Kimball Johnson <kimball@bowerham.net>
2013-04-10 22:40:27 +0100

    Modify for w34 reader

--- weigand.c
+++ weigand.c
@@ -12,13 +12,12 @@
 #include "boot.h"
 
 volatile int bit_count = 0;
-volatile unsigned char data[7];
+volatile unsigned char data[4];
 volatile int flg_readcard = 0;
 volatile int last_read;
 
 void RFID_Init(void)
 {
-    //_delay_ms(2000);	
     // Set D0 and D1 to inputs, enable pull up and enable their interupts
     // for DATA0/1
     bit_clear(DDRD, BIT(0));   
@@ -30,9 +29,7 @@ void RFID_Init(void)
 
     // Set D5 and D6 to outputs for LEDs
     bit_set(DDRD, BIT(5));
-    bit_set(DDRD, BIT(6));   
     bit_set(PORTD,BIT(5));
-    bit_set(PORTD,BIT(6));
 
     // Set C5 to output for buzzer
     bit_set(DDRC, BIT(5));
@@ -59,7 +56,7 @@ void data0_int(void)
     last_read = jiffies;
     set_data_bit_sc(data,bit_count,0);
     bit_count++;
-    _delay_us(100); // Delay to remove bounce
+    _delay_us(400); // Delay to remove bounce
 }
 
 void data1_int(void)
@@ -67,7 +64,7 @@ void data1_int(void)
     last_read = jiffies;
     set_data_bit_sc(data,bit_count,1);
     bit_count++;
-    _delay_us(100); // Delay to remove bounce
+    _delay_us(400); // Delay to remove bounce
 }
 
 void RFID_Task(void)
@@ -88,22 +85,6 @@ void RFID_Task(void)
             _delay_ms(500);
             bit_set(PORTD,BIT(5));
             break;
-        case 'r':
-        case 'R':
-            //toggle red led briefly (active low)
-            bit_clear(PORTD,BIT(6));
-            _delay_ms(500);
-            bit_set(PORTD,BIT(6));
-            break;
-        case 'o':
-        case 'O':
-            //toggle both leds briefly for orange
-            bit_clear(PORTD,BIT(6));
-            bit_clear(PORTD,BIT(5));
-            _delay_ms(500);
-            bit_set(PORTD,BIT(6));
-            bit_set(PORTD,BIT(5));
-            break;
         default:
             //Nothing
             break;
@@ -112,21 +93,27 @@ void RFID_Task(void)
     cli();
 
     //if it has been 50ms since we last had a rfid bit
-    if ((jiffies-last_read) > 50){
+    if ((jiffies-last_read) > 100){
         flg_readcard = 1;
     }
 
-    if (bit_count > 0 && (flg_readcard==1 || bit_count >= 56))
+    if (bit_count > 0 && (flg_readcard==1 || bit_count >= 32))
     {
-	    char buf[17];
+	    char buf[12];
         //Write code
-        snprintf(buf,17,"%.2x%.2x%.2x%.2x%.2x%.2x%.2x\r\n", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
-        CDC_Device_SendString(&VirtualSerial_CDC_Interface,buf);
-        CDC_Device_Flush(&VirtualSerial_CDC_Interface);
+        if (bit_count > 1)
+        {
+            snprintf(buf,12,"%.2x%.2x%.2x%.2xs\r\n", 
+                data[3], data[2], data[1], data[0]);
+            CDC_Device_SendString(&VirtualSerial_CDC_Interface,buf);
+            CDC_Device_Flush(&VirtualSerial_CDC_Interface);
+        }
         bit_count = 0;
         flg_readcard = 0;
-        for (int i=0;i<7;i++)
+        for (int i=0;i<4;i++)
+        {
             data[i]=0;
+        }
     }
     sei();
 }
